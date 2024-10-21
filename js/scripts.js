0b6fc659461437ca6ab89847c3708ec86c0506f5
Jas <50244053+JasDevelops@users.noreply.github.com>
2024-10-21 17:52:22 +0200

    Added pokemonList to scripts.js
    - added 3 objects to the array pokemonList

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+let pokemonList = [
+    {
+        name:'Bulbasaur',
+        height: 7,
+        types: ['grass', 'poison']
+    },
+    {
+        name:'Pidgey',
+        height: 3,
+        types: ['flying' , 'normal']
+
+    },
+    {
+        name:'Nidoking',
+        height: 14,
+        types: ['ground', 'poison']
+    }
+];
