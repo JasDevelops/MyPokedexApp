b853f67dabcf812d9f2d1d6b9fe1db045f49e419
Jas <50244053+JasDevelops@users.noreply.github.com>
2024-10-25 12:09:46 +0200

    Create IIEF
    * wrap pokemonList in IIFE
    * create public functions (getAll and add)
    * update forEach()-function to be able to access data (getAll)

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,41 @@
-// Array-list of Pokemons
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Pidgey',
-        height: 0.3,
-        types: ['flying', 'normal']
-
-    },
-    {
-        name: 'Nidoking',
-        height: 1.4,
-        types: ['ground', 'poison']
+// IIFE Array-list of Pokemons
+let pokemonRepository = (function (){
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Pidgey',
+            height: 0.3,
+            types: ['flying', 'normal']
+    
+        },
+        {
+            name: 'Nidoking',
+            height: 1.4,
+            types: ['ground', 'poison']
+        }
+    ];
+    function getAll (){
+        return pokemonList;
     }
-];
+    function add (pokemon){
+        pokemonList.push(pokemon);
+    }
+    return {
+        getAll: getAll,
+        add: add
+    };
+    
+})();
+
 
 // forEach ()-loop to iterate over each object within array
-function PokemonDetailsLoop(pokemon) {
-    let name = pokemon.name;
-    let height = pokemon.height;
+function PokemonDetailsLoop(details) {
+    let name = details.name;
+    let height = details.height;
     let text =
         height > 1
             ? `<span class="card__front--name">${name}</span> (height: ${height}) - Wow, that's big!`
@@ -30,4 +44,4 @@ function PokemonDetailsLoop(pokemon) {
     document.write(`<div class="card__front">${text}</div>`);
 }
 
-pokemonList.forEach(PokemonDetailsLoop);
+pokemonRepository.getAll().forEach(PokemonDetailsLoop);
