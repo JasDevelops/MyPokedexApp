5507b2a9abac70e18c19106a59f9414796963b60
Jas <50244053+JasDevelops@users.noreply.github.com>
2024-10-25 14:37:26 +0200

    Update scripts
    * changed double quotes to single for consistency
    * changed key types to type to make more sense

--- js/scripts.js
+++ js/scripts.js
@@ -4,36 +4,38 @@ let pokemonRepository = (function () {
         {
             name: 'Bulbasaur',
             height: 0.7,
-            types: ['grass', 'poison']
+            type: ['grass', 'poison']
         },
         {
             name: 'Pidgey',
             height: 0.3,
-            types: ['flying', 'normal']
+            type: ['flying', 'normal']
 
         },
         {
             name: 'Nidoking',
             height: 1.4,
-            types: ['ground', 'poison']
+            type: ['ground', 'poison']
         }
     ];
+    // create pokemonList accessible outside of the function
     function getAll() {
         return pokemonList;
     }
+    // add pokemon (items) if conditions are met
     function add(pokemon) {
-        let keysNeeded = ['name', 'height', 'types'];
+        let keysNeeded = ['name', 'height', 'type'];
         if (
             typeof pokemon === 'object' && // checks if item is an object
             Object.keys(pokemon).length === keysNeeded.length && // checks if amount of keys in item is the same as the amount defined in keysNeeded
             pokemon !== null && // prevent typeof null === 'object' quirk
             pokemon.name !== undefined && // checks if name is defined
             pokemon.height !== undefined && //checks if height is defined
-            pokemon.types !== undefined // checks if types is defined
+            pokemon.type !== undefined // checks if type is defined
         ) {
             pokemonList.push(pokemon); // add item if expectations are met
         } else {
-            console.error('Please provide an object with name, height and types properties') // print error in console, if expectations were not met
+            console.error('Please provide an object with name, height and type properties') // print error in console, if expectations were not met
         }
     }
     return {
@@ -47,10 +49,10 @@ function PokemonDetailsLoop(details) {
     let height = details.height;
     let text =
         height > 1
-            ? `<span class="card__front--name">${name}</span> (height: ${height}) - Wow, that's big!`
-            : `<span class="card__front--name">${name}</span> (height: ${height})`;
+            ? `<span class='card__front--name'>${name}</span> (height: ${height}) - Wow, that's big!`
+            : `<span class='card__front--name'>${name}</span> (height: ${height})`;
 
-    document.write(`<div class="card__front">${text}</div>`);
+    document.write(`<div class='card__front'>${text}</div>`);
 }
 
 pokemonRepository.getAll().forEach(PokemonDetailsLoop);
