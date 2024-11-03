1c000fc03a55a545e86879c5809c0e4c4c2a85c9
Jas <50244053+JasDevelops@users.noreply.github.com>
2024-11-03 11:57:10 +0100

    Add loadList()
    * Add loadList()-function to IIEF yo fetch Pokémon-list from external API

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 let pokemonRepository = (function () {
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
     // create pokemonList accessible outside of the function
     function getAll() {
         return pokemonList;
@@ -47,12 +48,37 @@ let pokemonRepository = (function () {
         console.log(pokemon);
     }
 
+    // Function to fetch list of Pokémon from API
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
+
+
+    pokemonRepository.loadList().then(function () {
+        // Now the data is loaded!
+        pokemonRepository.getAll().forEach(function (pokemon) {
+            pokemonRepository.addListItem(pokemon);
+        });
+    });
     // make them accessible from outside of the function
     return {
         getAll: getAll,
         add: add,
         addListItem: addListItem,
         showDetails: showDetails
+        loadList: loadList,
     }
 })();
 // filter() - function : filter by name
